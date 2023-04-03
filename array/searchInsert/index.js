a9d9bd979263f5d451e3125c4ebb205ec8c0886e
vd89 <dixitvora@gmail.com>
2023-04-03 21:11:32 +0530

    35_search_dax: change

--- array/searchInsert/index.js
+++ array/searchInsert/index.js
@@ -4,18 +4,18 @@
  * @return {number}
  */
 var searchInsert = function(nums, target) {
-  let l = 0
-  let r = nums.length - 1;
-  while (l <= r ) {
-    let mid = Math.floor((l+r)/2);
-    if (nums[mid] === target) return mid
+  let l = 0;
+  let r = nums.length -1;
+   while (l <= r) {
+    let mid = Math.floor((r+l)/2)
+    if(nums[mid] === target) return mid
+
     if (target < nums[mid]) {
       r = mid -1
     } else {
       l = mid +1
     }
-  }
-
+   }
   return l;
 };
 
