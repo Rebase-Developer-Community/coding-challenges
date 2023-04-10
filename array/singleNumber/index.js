cfb55de51f4ecff3d069a16fe8e7eafa8d49b3b3
vd89 <dixitvora@gmail.com>
2023-04-10 20:26:36 +0530

    #29: added the solution as well

---
+++ array/singleNumber/index.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+
+const singleNumber = function(nums) {
+  let xor = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    xor ^= nums[i]
+  }
+  return xor
+};
+
+module.exports = singleNumber
