074a2a974a36d8ffebfdf10dddad57f4b8e214f5
vd89 <dixitvora@gmail.com>
2023-04-12 20:36:45 +0530

    #34: solved the problem

--- array/summaryRanges/index.js
+++ array/summaryRanges/index.js
@@ -5,7 +5,24 @@
 
 
 const summaryRanges = function(nums) {
+  if (!nums.length) return []
 
+  const output = []
+  let currentValue = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!i || nums[i] === currentValue[currentValue.length -1] + 1 ) {
+      currentValue.push(nums[i])
+    }else{
+      output.push(currentValue.length > 1 ? `${currentValue[0]}->${currentValue[currentValue.length - 1]}` : `${currentValue}`)
+      currentValue = [nums[i]]
+    }
+  }
+
+  output.push(currentValue.length > 1 ? `${currentValue[0]}->${currentValue[currentValue.length -1]}`: `${currentValue}`)
+
+
+  return output
 };
 
 module.exports = summaryRanges
