d06b0340f8964fe83d9f828b0bd74e61ddce1b93
vd89 <dixitvora@gmail.com>
2023-04-17 21:10:33 +0530

    #39: added the fix

--- array/moveZeroes/index.js
+++ array/moveZeroes/index.js
@@ -6,6 +6,14 @@
 
 const moveZeroes = function(nums) {
 
+  let size = nums.length -1;
+  for (let i = size; i >= 0; i--) {
+   if (nums[i] ===0) {
+    nums.splice(i,1);
+    nums.push(0)
+   }
+  }
+  return nums
 };
 
 
